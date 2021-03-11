0dfa655eeae12649b3ace834536e7b7023e17892
pasrthaSdeb <parthadeb95@gmail.com>
2021-03-11 10:27:37 +0600

    hackerrank problem - a very big sum

--- a_very_big_sum.py
+++ a_very_big_sum.py
@@ -1,18 +1,29 @@
-# n = int(input('enter the range of the array: '))
-# print(n)
+# !/bin/python3
 
-# test_list= []
-# for i in range(n):
-#     x = int(input('enter number: '))
-#     # print(x)
-#     test_list.append(x)
-
-# print(test_list)
+import math
+import os
+import random
+import re
+import sys
 
 
 # Complete the aVeryBigSum function below.
-def aVeryBigSum(ar, ar_count):
+def aVeryBigSum(ar):
     sum = 0
-    for i in range(ar_count):
-        sum = sum + int(ar[i])
+    for i in range(len(ar)):
+        sum += ar[i]
     return sum
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
